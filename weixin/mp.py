8dbf5b0fa303f0d9730002e1454298fc2ab2a0cc
chengluoran <chengluoran@qq.com>
2018-07-12 19:04:46 +0800

    修改json编码

--- weixin/mp.py
+++ weixin/mp.py
@@ -98,7 +98,7 @@ class WeixinMP(object):
         token and params.setdefault("access_token", self.access_token)
         headers = {}
         if json_encode:
-            data = json.dumps(data, ensure_ascii=False)
+            data = json.dumps(data, ensure_ascii=False).encode("utf-8")
             # data = json.dumps(data)
             headers["Content-Type"] = "application/json;charset=UTF-8"
         # print url, params, headers, data
